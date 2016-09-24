82d70b066a7fba3bbdc3fa076f0500d5064592c1
Susindaran Elangovan <susindaran.e@flipkart.com>
2016-09-24 20:08:24 +0530

    Added recycler view with cards

---
+++ app/src/main/java/com/betadevels/autosound/adapters/TriggerCardsAdapter.java
@@ -0,0 +1,8 @@
+package com.betadevels.autosound.adapters;
+
+/**
+ * Created by susindaran.e on 18/09/16.
+ */
+public class TriggerCardsAdapter
+{
+}
---
+++ app/src/main/java/com/betadevels/autosound/resources/Utilities.java
@@ -0,0 +1,8 @@
+package com.betadevels.autosound.resources;
+
+/**
+ * Created by susindaran.e on 18/09/16.
+ */
+public class Utilities
+{
+}
