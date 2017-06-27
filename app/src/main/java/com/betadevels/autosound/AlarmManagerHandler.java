11a9386871d5a402259a204910afea528ede04b5
Susindaran Elangovan <susindaran94@gmail.com>
2017-06-26 19:30:54 -0500

    Using exact alarm if possible for more accurate triggers

--- app/src/main/java/com/betadevels/autosound/AlarmManagerHandler.java
+++ app/src/main/java/com/betadevels/autosound/AlarmManagerHandler.java
@@ -4,6 +4,7 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 import com.betadevels.autosound.resources.Constants;
 
@@ -35,7 +36,14 @@ public class AlarmManagerHandler
         }
         else
         {
-            alarmManager.set( AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent );
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+            {
+                alarmManager.setExact( AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent );
+            }
+            else
+            {
+                alarmManager.set( AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent );
+            }
         }
     }
 
